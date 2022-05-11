58fdd5388012cd37cad32720d46c53c0aaf4f30f
Brian D <bdobbins@gmail.com>
2022-05-11 14:13:40 +0000

    Need passlib for users script, which is going to be part of conda install - skipping for this test phase

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,7 +5,7 @@
 cd /root
 
 if [[ "compute" == $2 ]]; then
-  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py $1 $2 > users.log
+#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py $1 $2 > users.log
 else
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/os.sh
   sh os.sh > os.log
@@ -13,6 +13,6 @@ else
   sh intel.sh > intel.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/opt_ncar.sh
   sh opt_ncar.sh > opt_ncar.log
-  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py
-  python3 users.py $1 > users.log
+#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py
+#  python3 users.py $1 > users.log
 fi
