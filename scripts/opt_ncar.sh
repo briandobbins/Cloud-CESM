e52bc5551473c900f130ba58fa8fa25be4cc3a1f
Brian D <bdobbins@gmail.com>
2022-05-11 14:48:59 +0000

    Fixed opt_ncar to use public S3 bucket until FTP servers are fixed

--- scripts/opt_ncar.sh
+++ scripts/opt_ncar.sh
@@ -2,6 +2,11 @@
 
 mkdir -p /opt/ncar
 cd /opt/ncar
-aws s3 cp --recursive s3://cesm-software/software ./software
-aws s3 cp --recursive s3://cesm-software/esmf ./esmf
+wget https://cesm-software.s3.us-east-2.amazonaws.com/esmf.tar.gz
+tar zxvf esmf.tar.gz
+rm -f esmf.tar.gz
+
+wget https://cesm-software.s3.us-east-2.amazonaws.com/software.tar.gz
+tar zxvf software.tar.gz
+rm -f software.tar.gz
 
