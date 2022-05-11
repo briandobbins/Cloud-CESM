13fbdd7f66b0732af6e80f77c8ffd3bb099f9091
Brian D <bdobbins@gmail.com>
2022-05-11 20:24:43 +0000

    Fix for conda

--- scripts/conda.sh
+++ scripts/conda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.11.0-Linux-x86_64.sh
-bash -b -p /opt/ncar/conda
+bash Miniconda3-py39_4.11.0-Linux-x86_64.sh -b -p /opt/ncar/conda
 
 # Add to path:
 echo 'export PATH=/opt/ncar/conda/bin:${PATH}' > /etc/profile.d/conda.sh
