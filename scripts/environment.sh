1b2b2bd95f517de2c76fede100d71238f7743625
Brian D <bdobbins@gmail.com>
2022-05-11 21:40:30 +0000

    Fix

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -7,18 +7,20 @@ EOF
 
 # Set up the CESM profile:
 cat << EOF > /etc/profile.d/cesm.sh
-export PATH=/opt/ncar/cesm/cime/scripts:${PATH}
+#export PATH=/opt/ncar/cesm/cime/scripts:${PATH}
 export CIME_MACHINE=aws-hpc6a
 export LD_LIBRARY_PATH=/opt/ncar/software/lib:${LD_LIBRARY_PATH}
 
-module load libfabric-aws
+
 export OMP_NUM_THREADS=1
 export I_MPI_OFI_LIBRARY_INTERNAL=0
 export I_MPI_FABRICS=ofi
 export I_MPI_OFI_PROVIDER=efa
 
-export PATH=${PATH}:/opt/ncar/software/bin
-export PATH=${PATH}:/opt/ncar/conda/bin
+export PATH=/opt/ncar/software/bin:${PATH}
+export PATH=/opt/ncar/conda/bin:${PATH}
+
+export LD_LIBRARY_PATH=/opt/amazon/efa/lib64:${LD_LIBRARY_PATH}
 
 export ESMFMKFILE=/opt/ncar/esmf/lib/esmf.mk
 EOF
