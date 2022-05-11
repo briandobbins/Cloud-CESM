18171723d4cb5ede758a36588ae3bd0eb6fba03f
Brian D <bdobbins@gmail.com>
2022-05-11 21:44:12 +0000

    Fix

--- scripts/os.sh
+++ scripts/os.sh
@@ -4,7 +4,7 @@
 # consistency with the container setup, so duplicates will just be skipped)
 yum -y upgrade
 yum -y install vim emacs-nox git subversion which sudo csh make m4 cmake wget file byacc curl-devel zlib-devel
-yum -y install perl-XML-LibXML gcc-gfortran gcc-c++ dnf-plugins-core python3 perl-core ftp numactl-devel
+yum -y install perl-XML-LibXML gcc-gfortran gcc-c++ dnf-plugins-core perl-core ftp numactl-devel
 
 # Set up the 'python' alias to point to Python3 -- this is going away for newer CESM releases, I think?
 #ln -s /usr/bin/python3 /usr/bin/python
