901aef8bbf19fb5737ff7e59718c4014a3c10aed
Brian D <bdobbins@gmail.com>
2022-05-11 19:32:16 +0000

    Fix

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,7 +5,7 @@
 cd /root
 
 if [[ "compute" == $2 ]]; then
-  aws s3 cp cesmapi-f72c5aee-5bc9-47ec-afb3-85431601d3f8/config.json .
+  aws s3 cp s3://cesmapi-f72c5aee-5bc9-47ec-afb3-85431601d3f8/config.json .
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py config.json > users.log
 else
 wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/os.sh
@@ -15,6 +15,6 @@ sh os.sh > os.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/opt_ncar.sh
   sh opt_ncar.sh > opt_ncar.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py 
-  aws s3 cp cesmapi-f72c5aee-5bc9-47ec-afb3-85431601d3f8/config.json .
+  aws s3 cp s3://cesmapi-f72c5aee-5bc9-47ec-afb3-85431601d3f8/config.json .
   python3 users.py config.json head > users.log
 fi
