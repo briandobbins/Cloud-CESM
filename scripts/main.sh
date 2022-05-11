ffc019767274c2b8a9c82167bd4fcf6d63c60b52
Brian D <bdobbins@gmail.com>
2022-05-11 14:12:09 +0000

    Test of main script

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,16 +4,15 @@
 
 cd /root
 
-#if [[ "compute" == $2 ]]; then
-#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py $1 $2
-#else
-#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/os.sh
-#  sh os.sh
-#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/intel.sh
-#  sh intel.sh
-#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/opt_ncar.sh
-#  sh opt_ncar.sh
-#  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py
-#  python3 users.py $1
-#
-#fi
+if [[ "compute" == $2 ]]; then
+  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py $1 $2 > users.log
+else
+  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/os.sh
+  sh os.sh > os.log
+  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/intel.sh
+  sh intel.sh > intel.log
+  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/opt_ncar.sh
+  sh opt_ncar.sh > opt_ncar.log
+  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py
+  python3 users.py $1 > users.log
+fi
