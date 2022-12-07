dcb70d37768d1a91741a0d97adb2518cb70daa22
Brian D <bdobbins@ucar.edu>
2022-12-07 14:58:58 +0000

    This version just downloads the scripts to trace an error

--- scripts/main.sh
+++ scripts/main.sh
@@ -11,24 +11,20 @@ if [[ "compute" == $2 ]]; then
   /opt/ncar/conda/bin/python3 users.py config.json compute > users.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/environment.sh
   sh environment.sh > environment.log
-  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/glade_symlink.sh
-  sh glade_symlink.sh > environment.log
 else
   mkdir -p /opt/ncar/config
   chmod go-rwx /opt/ncar/config
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/os.sh
-  sh os.sh > /opt/ncar/config/os.log
+  #sh os.sh > /opt/ncar/config/os.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/conda.sh
-  sh conda.sh > /opt/ncar/config/conda.log
+  #sh conda.sh > /opt/ncar/config/conda.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/intel.sh
-  sh intel.sh > /opt/ncar/config/intel.log
+  #sh intel.sh > /opt/ncar/config/intel.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/opt_ncar.sh
-  sh opt_ncar.sh > /opt/ncar/config/opt_ncar.log
+  #sh opt_ncar.sh > /opt/ncar/config/opt_ncar.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/environment.sh
-  sh environment.sh > /opt/ncar/config/environment.log
-  wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/glade_symlink.sh
-  sh glade_symlink.sh > environment.log
+  #sh environment.sh > /opt/ncar/config/environment.log
   wget https://raw.githubusercontent.com/briandobbins/Cloud-CESM/master/scripts/users.py 
   aws s3 cp $1/config.json .
-  /opt/ncar/conda/bin/python3 users.py config.json head > users.log
+  #/opt/ncar/conda/bin/python3 users.py config.json head > users.log
 fi
