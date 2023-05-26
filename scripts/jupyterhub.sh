08f8fdc5a3497ac00c9c096abe2d985afddeeec2
Brian Dobbins <bdobbins@gmail.com>
2023-05-26 16:17:11 +0000

    fix for JH

--- scripts/jupyterhub.sh
+++ scripts/jupyterhub.sh
@@ -10,15 +10,16 @@ fi
 
 # OK, it's enabled, so let's set it up:
 
-groupadd -g 801 jupyterhub
 adduser -c "JupyterHub" -d /local/jupyter -s /bin/bash -u 801 -g jupyterhub jupyter
 
 
-runuser -l jupyter -c 'conda install -y -n jupyterhub jupyterhub jupyterlab notebook configurable-http-proxy'
+runuser -l jupyter -c 'conda create -y -n jupyterhub'
+runuser -l jupyter -c 'conda install -y -n jupyterhub jupyterhub jupyterlab notebook configurable-http-proxy ipywidgets'
 runuser -l jupyter -c 'conda install -y -n jupyterhub -c conda-forge sudospawner'
+runuser -l jupyter -c 'npm install -g configurable-http-proxy'
+
 
 cat << EOF > /etc/sudoers.d/jupyterhub
-____________________Begin jupytersudoers file____________________
 # the command(s) the Hub can run on behalf of the above users without needing a password
 # the exact path may differ, depending on how sudospawner was installed
 
@@ -26,11 +27,29 @@ Cmnd_Alias JUPYTER_CMD = /local/jupyter/.conda/envs/jupyterhub/bin/sudospawner
 
 # actually give the Hub user permission to run the above command on behalf
 # of the above users without prompting for a password
+%jupyterhub ALL=(ALL:ALL) NOPASSWD: JUPYTER_CMD
 
-%jupyterhub ALL=(jupyter) /usr/bin/sudo
-jupyter ALL=(%jupyterhub) NOPASSWD:JUPYTER_CMD
 
-_____________________End jupytersudoers file____________________
 EOF
+
+# Generate the config
+runuser -l jupyter -c 'jupyterhub --generate-config'
+
+cat << EOF >> /home/jupyter/jupyterhub_config.py
+c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.hub_connect_ip = '0.0.0.0'
+c.Authenticator.admin_users = {'jupyter','bdobbins'}
+c.JupyterHub.spawner_class = 'sudospawner.SudoSpawner'
+c.Spawner.cmd = '/local/jupyter/.conda/envs/jupyterhub/bin/sudospawner'
+
+c.JupyterHub.hub_port = 8880
+c.JupyterHub.port = 443
+c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
+
+#c.JupyterHub.ssl_cert = '/etc/letsencrypt/live/site/fullchain.pem'
+#c.JupyterHub.ssl_key = '/etc/letsencrypt/live/site/privkey.pem'
+
+EOF
+
 
 
